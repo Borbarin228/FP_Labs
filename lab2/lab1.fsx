2f45474fbcd41a23270372a341c550cc99761b2b
Borbarin228 <114693651+Borbarin228@users.noreply.github.com>
2025-03-01 09:44:31 +0500

    3 сдал ща 4

--- lab2/lab1.fsx
+++ lab2/lab1.fsx
@@ -1,23 +1,31 @@
+let mutable inputList = []
 
-let area a b c =
-    let s = (fun a b c ->(a + b + c) / 2.0)a b c
-    sqrt ((fun a b c s -> s * (s - a) * (s - b) * (s - c))a b c s)
+let parseText (text:string) = 
+    if text.Length<=3 then text else text.Substring(0,3)
 
-let mutable list = []
+let sumUnits (unit:string) =    
+      System.Int32.Parse(string unit[0])+System.Int32.Parse(string unit[1])+System.Int32.Parse(string unit[2])
+      
 
-let main argv =
+let checkDevide(value:string) = 
+    if (sumUnits value) = (sumUnits value) /3*3 then
+        printfn "%s - divide 3" value
+    else 
+        printfn "%s - not divide 3" value
+    if (sumUnits value) = (sumUnits value) /9*9 then
+        printfn "%s - divide 9" value
+    else 
+        printfn "%s - not divide 9" value
+       
     
+
+let main argv = 
     printf "Input a: "
-    list <-(float (System.Console.ReadLine()))::list
-
+    inputList <-(parseText (string (System.Console.ReadLine())))::inputList
     printf "Input b: "
-    list <-(float (System.Console.ReadLine()))::list
-
-    printf "Input c: "
-    list <-(float (System.Console.ReadLine()))::list
-
+    inputList <-(parseText (string (System.Console.ReadLine())))::inputList
     
-    printfn "Perimeter: %f" ((fun a b c -> a + b + c)list[0] list[1] list[2])
-    printfn "Area: %f" (area list[0] list[1] list[2])
-
-main() 
+    checkDevide inputList[1]
+    checkDevide inputList[0]
+    
+main()
